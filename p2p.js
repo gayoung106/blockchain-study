c24924e63a4193de9393b2b40a514ee359721ad7
SongGayoung <xjongamx@kyonggi.ac.kr>
2023-09-27 22:00:59 +0900

    4교시: 소켓 진행중

--- p2p.js
+++ p2p.js
@@ -1,7 +1,8 @@
-const { WebSocketServer } = require("ws");
-
+const { WebSocketServer, WebSocket } = require("ws");
+const MessageType = require("./msg");
 class P2PServer {
-  constructor() {
+  constructor(chain) {
+    this.chain = chain;
     this.sockets = [];
   }
   //계속해서 듣고 있는 상태
@@ -12,9 +13,68 @@ class P2PServer {
       const address = req.socket.remoteAddress;
       const port = req.socket.remotePort;
       console.log(`Success connection!! ${address}:${port}`);
-      this.sockets.push(socket);
+      this.connectSocket(socket, "server");
       //연결된 소켓을 sockets안에 배열에 넣어둠
     });
+  }
+
+  connectToPeer(newPeer) {
+    const socket = new WebSocket(newPeer);
+    socket.on("open", () => {
+      console.log("open");
+      this.connectSocket(socket, "client");
+    });
+  }
+
+  connectSocket(socket, name) {
+    console.log(name);
+    this.sockets.push(socket);
+    this.messageHandler(socket);
+    const data = {
+      type: MessageType.latest_block,
+      payload: name,
+    };
+    this.send(socket, data);
+  }
+
+  getSockets() {
+    return this.sockets;
+  }
+
+  messageHandler(socket) {
+    const cb = (message) => {
+      const result = JSON.parse(message.toString());
+      switch (result.type) {
+        case MessageType.latest_block:
+          const msg1 = {
+            type: MessageType.all_block,
+            payload: this.chain.getLastBlock(),
+          };
+          this.send(socket, msg1);
+          break;
+        case MessageType.all_block:
+          const msg2 = {
+            type: MessageType.receivedChain,
+            payload: this.chain.blockchain,
+          };
+          const receivedBlock = result.payload;
+          const isSuccess = this.chain.addBlock(receivedBlock);
+          if (isSuccess) break;
+          this.send(socket, msg2);
+          break;
+        case MessageType.receivedChain:
+          const receivedChain = result.payload;
+          // this.chain.hanleChainResponse(receivedChain, this);
+          break;
+        case MessageType.receivedTx:
+          break;
+      }
+    };
+    socket.on("message", cb);
+  }
+
+  send(socket, message) {
+    socket.send(JSON.stringify(message));
   }
   //sockets안에 있는 배열은
   //나와 연결된 모든 노드들에게 내용을 전파하기 위함
--- test.js
+++ test.js
@@ -2,9 +2,11 @@ const Chain = require("./chain");
 const P2PServer = require("./p2p");
 
 function main() {
+  const chain = new Chain();
   const p2p = new P2PServer();
 
   p2p.listen();
+  p2p.connectToPeer("ws://localhost:7545");
 }
 
 main();
