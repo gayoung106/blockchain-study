054f2291dd4662b85c35d429bca133d6cef1af4e
SongGayoung <xjongamx@kyonggi.ac.kr>
2023-09-27 20:15:58 +0900

    2교시: 유효성 검증

--- block.js
+++ block.js
@@ -1,43 +1,54 @@
 const { SHA256 } = require("crypto-js");
 const merkle = require("merkle");
 
-// npm install crypto-js merkle 라이브러리 설치
 class Block {
-  constructor(previoushBlock, transactions) {
-    this.index = (previoushBlock?.index || 0) + 1; //블록넘버링 : 이전블락의 +1
-    // this.version = "1.0.0"; //블록버전
-    this.timestamp = new Date().getTime(); //타임스탬프
-    this.previousHash = previoushBlock?.hash; // 이전블록해시
-    this.nonce = 0; // 해시퍼즐-정답
-    this.difficulty = 0; // 해시퍼즐
-    // this.data; //일반 데이터
-    this.hash; //현재블록의 해시값
-    this.merkleRoot = Block.getMerkleRoot(transactions);
-    this.transactions = transactions; // 트랜잭션 모음
+  /**
+   * Block 인스턴스 시 초기화
+   * @param {Block} previousBlock
+   * @param {object[]} transactions
+   */
+  constructor(previousBlock, transactions) {
+    this.index = (previousBlock?.index || 0) + 1; // 블록넘버
+    this.timestamp = new Date().getTime(); // 타임스탬프
+    this.previousHash = previousBlock?.hash; // 이전 블록해시
+    this.nonce = 0; // 해시퍼즐정답
+    this.difficulty = 1; // 해시퍼즐
+    this.hash = 0; // 현재 블록 해시값
+    this.merkleRoot = Block.getMerkleRoot(transactions); // 머클루트
+    this.transactions = transactions; // 트랜잭션 모음 body
   }
-  //함수만들기
-  //데이터에 따라서 머클루트를 가져오는 함수
+
+  /**
+   * 트랜잭션 데이터 머클루트해시
+   * @param {object[]} data
+   * @returns 머클루트해시
+   */
   static getMerkleRoot(data) {
     const merkleTree = merkle("sha256").sync(data);
     return merkleTree.root();
   }
 
-  //머클루트와 해시에 대한 내용을 가져오는 함수
+  /**
+   * SHA256 해시 만들기 - nonce를 통해 채굴시 사용
+   * @param {Block} block
+   * @returns SHA256 해시
+   */
   static createBlockHash(block) {
     const { index, timestamp, merkleRoot, previousHash, nonce } = block;
     const values = `${index}${timestamp}${merkleRoot}${previousHash}${nonce}`;
     return SHA256(values).toString();
   }
 
+  /**
+   * 제네시스블록 가져오기
+   * @returns 제네시스블록
+   */
   static getGenesis() {
     const transactions = ["2차 구제금융"];
-    return new Block({ index: 0, hash: 0x0 }, transactions);
+    const genesisBlock = new Block({ index: 0, hash: 0x0 }, transactions);
     genesisBlock.hash = Block.createBlockHash(genesisBlock);
     return genesisBlock;
   }
 }
-//블럭을 하나 생성
-
-console.log(Block.getGenesis());
 
 module.exports = Block;
--- chain.js
+++ chain.js
@@ -1,84 +1,86 @@
-//npm install bn.js
 const { BN } = require("bn.js");
 const Block = require("./block");
 
 class Chain {
-  //난이도, 타겟, 마이닝
-  constructor() {
-    this.blocks = [Block.getGenesis()]; // 블럭들이 모이는 곳
-    this.mempool = []; // 트랜잭션들이 모이는 곳
-  }
-  //멤풀에 트랜잭션 추가
   /**
-   *
-   * @param {object} tx
+   * 체인 인스턴스 초기화 값
+   */
+  constructor() {
+    this.blocks = [Block.getGenesis()]; // 블록 모이는 곳
+    this.mempool = []; // tx 모이는 곳
+  }
+  /**
+   * 멤풀에 트랜잭션 추가
+   * @param {object} tx 트랜잭션
    */
   addTx(tx) {
     this.mempool.push(tx);
   }
   /**
-   * 체인에 블록추가
-   * @param {block} block
+   * 체인에 블록 추가
+   * @param {Block} block 블록
    */
   addBlock(block) {
-    //TODO: 블록 유효성 검증을 진행해야함
+    //TODO block 유효성 검증을 진행해야함
+    const lastBlock = this.getLastBlock();
 
-    this.blocks.push(block);
+    if (this.isValidBlock(lastBlock, block)) {
+      this.blocks.push(block);
+    } else {
+      console.log("유효하지 않은 블록입니다.");
+    }
   }
-  //채굴
+  /**
+   * 채굴
+   */
   mining() {
-    console.log("채굴시작");
-    //코인베이스 트랜잭션
+    console.log("채굴 시작!");
+    // 코인베이스 트랜잭션
     const coinbaseTx = { from: "COINBASE", to: "MINER", value: 50 };
-    // 트랜잭션 리스트 생성 - body
+    // 트랜잭션 리스트를 만듦 - Body
     const txList = [coinbaseTx, ...this.mempool];
     // 멤풀을 비워줌
     this.mempool = [];
-    // 마지막 블록을 가져옴
+    // 마지막(최신) 블록을 가져옴
     const lastBlock = this.getLastBlock();
     // 새로운 블록 생성
     const newBlock = new Block(lastBlock, txList);
     // 새로운 블록 난이도 셋팅
     newBlock.difficulty = this.getDifficulty(lastBlock.difficulty);
-    // 타겟값 조회
+    // 타겟(목표값) 조회
     const target = this.getTarget(newBlock.difficulty);
-
-    // 목표값과 해시값을 비교하여 해시퍼즐 정답을 생성
+    // 목표값과 해시값을 비교하여 해시퍼즐 정답을 만듦
     while (!(Block.createBlockHash(newBlock) <= target)) {
       newBlock.nonce++;
-
-      console.log(newBlock.nonce, Block.createBlockHash(newBlock));
     }
-    //해시퍼즐 정답을 만든 해시를 기존 블록에 셋팅
+    // 해시퍼즐 정답을 만든 해시를 기존 블록에 셋팅
     newBlock.hash = Block.createBlockHash(newBlock);
-    //새로만든 블록 출력
+    // 새로만든 블록 출력
     console.log(newBlock);
-    console.log("채굴성공!");
-    //- 체인에 블록을 연결함
+    console.log("채굴 성공!");
+    // 블록체인에 블록을 연결
     this.addBlock(newBlock);
   }
 
   /**
-   * 난이도 구하기 -> 자가제한 시스템
+   * 난이도 구하기 -> 자가제한시스템
    * @param {number} difficulty 난이도
    * @returns 자가제한 시스템이 적용 된 난이도
    */
   getDifficulty(difficulty) {
     const lastBlock = this.getLastBlock();
-    /** 일관된 블록 채굴 시간을 유지하기 위해 수행 */
-    //라스트블락의 인덱스가 만약에 11이면 모듈러 값은 1이 됨, 그러니깐 11, 21, 31 ...단위는 if문안에 들어가서 동작하게 됨
-    //따라서 1-10은 바깥으로 나감 = 기존의 난이도를 주면됨
-    //즉, 나머지가 1인 경우 난이도를 체크해서 변경해줌
-    if (lastBlock.index > 1 && lastBlock.index % 10 === 1) {
+    const SECONDS = 10; // 초
+    const BLOCK_NUMBER = 10;
+    const MUL = 4;
+    // 10의 배수일 경우 난이도를 체크해서 변경해준다
+    if (lastBlock.index > 0 && lastBlock.index % BLOCK_NUMBER === 0) {
       console.log("난이도 조절 시작");
-      //이전타임이 필요함
-      let prevTime = this.blocks[this.blocks.length - 11].timestamp;
-      //마지막(최근)타임
+      let prevTime = this.blocks[this.blocks.length - BLOCK_NUMBER].timestamp;
       let lastTime = lastBlock.timestamp;
-      //마지막 10개 블록을 채굴하는 데 걸린 평균 시간(평균시간은 채굴 난이도 조정 여부를 결정)
-      let avgTime = (lastTime - prevTime) / 11 / 1000;
-      //20초 미만이면 4로 설정되어 난이도가 높아지고, 20이상이면 1/4로 설정되어 난이도를 감소
-      let multiple = avgTime < 20 ? 4 : 1 / 4;
+      let avgTime = (lastTime - prevTime) / BLOCK_NUMBER / 1000;
+      console.log("평균시간", avgTime);
+      let multiple = avgTime < SECONDS ? MUL : 1 / MUL;
+      console.log(avgTime < SECONDS ? "난이도를 올림" : "난이도를 낮춤");
       difficulty = multiple * difficulty;
       console.log("변경된 난이도", difficulty);
       console.log("난이도 조절 끝");
@@ -87,12 +89,12 @@ class Chain {
   }
 
   /**
-   * 타켓구하기
+   * 타겟(목표값) 구하기
    * @param {number} difficulty 난이도
    * @returns 목표값
    */
   getTarget(difficulty) {
-    let bits = this.difficultyToBits(difficulty);
+    let bits = this.difficultToBits(difficulty);
     let bits16 = parseInt("0x" + bits.toString(16), 16);
     let exponents = bits16 >> 24;
     let mantissa = bits16 & 0xffffff;
@@ -103,12 +105,12 @@ class Chain {
   }
 
   /**
-   * 난이도를 통해서 비트구하기(타겟=목표값을 구하기 위함이 목적)
-   * @param {*} difficulty
-   * @returns
+   * 난이도를 통해서 비트 구하기
+   * @param {number} difficulty 난이도
+   * @returns 난이도 비트
    */
-  difficultyToBits(difficulty) {
-    const maximumTarget = "0x0ffff000000" + "0".repeat(64 - 12);
+  difficultToBits(difficulty) {
+    const maximumTarget = "0x00ffff000000" + "0".repeat(64 - 12);
     const difficulty16 = difficulty.toString(16);
     let target = parseInt(maximumTarget, 16) / parseInt(difficulty16, 16);
     let num = new BN(target.toString(16), "hex");
@@ -120,7 +122,6 @@ class Chain {
     } else {
       compact = num.ushrn(8 * (nSize - 3)).toNumber();
     }
-
     if (compact & 0x800000) {
       compact >>= 8;
       nSize++;
@@ -133,16 +134,31 @@ class Chain {
     return parseInt(bits.toString(10));
   }
 
-  //마지막 블록(=최신블록)
-
+  /**
+   * 체인에 연결된 마지막 블록
+   * @returns 마지막 블록
+   */
   getLastBlock() {
     return this.blocks[this.blocks.length - 1];
   }
 
-  //유효성검증-블록검증
-  isValidBlock() {}
-
-  //유효성검증-블록체인검증
+  /**
+   *유효성 블록 검증
+   * @param {Block} preBlock
+   * @param {Block} newBlock
+   * @returns
+   */
+  isValidBlock(preBlock, newBlock) {
+    //1은 genesisBlock을 의미하기 때문에, 유효성 검증을 할 필요 없음
+    if (preBlock.index === 1) return 1;
+    //3가지 조건이 true여야 함
+    return (
+      preBlock.index !== 1 &&
+      preBlock.hash === newBlock.previousHash &&
+      Block.createBlockHash(newBlock) === newBlock.hash
+    );
+  }
+  // 유효성 블록체인 검증
   isValidBlockchain() {}
 }
 
